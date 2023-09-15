b9ab110d4c5a374c6011cec79c9b3040a503d2c1
Carter <carterleebainter@gmail.com>
2023-09-15 16:05:06 -0400

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+      int spring = 400;
+      int summer = 500;
+      int fall = 300;
+      int winter = 400;
+      int totalCost = 0;
+
+      totalCost = spring + summer + fall + winter;
+
+        System.out.println("Spring total: " + spring + " , Summer total: " + summer + ", fall total: " + fall + " , Winter total: " + winter);
+        System.out.println("Your total yearly cost is: " + totalCost);
     }
 }
